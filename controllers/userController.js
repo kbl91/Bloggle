644352734273ff2be61488d8db568e688aa3efb9
kbl91 <kbl91@yahoo.com>
2023-04-27 17:40:09 -0400

    install multer package

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,7 +3,7 @@ const User = require('../Models/User')
 exports.login = function (req, res) {
   let user = new User(req.body)
   user.login().then(function (result) {
-    req.session.user = { username: user.data.username }
+    req.session.user = { avatar: user.avatar, username: user.data.username }
     req.session.save(function () {
       res.redirect('/')
     })
@@ -21,18 +21,18 @@ exports.logout = function (req, res) {
   })
 }
 
-exports.register = function(req, res) {
+exports.register = function (req, res) {
   let user = new User(req.body)
   user.register().then(() => {
-    req.session.user = {username: user.data.username}
-    req.session.save(function() {
+    req.session.user = { username: user.data.username, avatar: user.avatar }
+    req.session.save(function () {
       res.redirect('/')
     })
   }).catch((regErrors) => {
-    regErrors.forEach(function(error) {
+    regErrors.forEach(function (error) {
       req.flash('regErrors', error)
     })
-    req.session.save(function() {
+    req.session.save(function () {
       res.redirect('/')
     })
   })
@@ -40,8 +40,8 @@ exports.register = function(req, res) {
 
 exports.home = function (req, res) {
   if (req.session.user) {
-    res.render('home-dashboard', { username: req.session.user.username })
+    res.render('home-dashboard', { username: req.session.user.username, avatar: req.session.user.avatar })
   } else {
-    res.render('home-guest', { errors: req.flash('errors'),regErrors: req.flash('regErrors') })
+    res.render('home-guest', { errors: req.flash('errors'), regErrors: req.flash('regErrors') })
   }
 }
