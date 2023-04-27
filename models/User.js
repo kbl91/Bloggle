3cb72336eb39298dcf801ba76ef46c17ed70a7c6
kbl91 <kbl91@yahoo.com>
2023-04-27 12:10:35 -0400

    styling with nes.css

--- models/User.js
+++ models/User.js
@@ -21,14 +21,29 @@ User.prototype.cleanUp = function () {
 }
 
 User.prototype.validate = function () {
-    if (this.data.username == "") { this.errors.push("You must provide a username.") }
-    if (this.data.username != "" && !validator.isAlphanumeric(this.data.username)) { this.errors.push("Username can only contain letters and numbers.") }
-    if (!validator.isEmail(this.data.email)) { this.errors.push("You must provide a valid email address.") }
-    if (this.data.password == "") { this.errors.push("You must provide a password.") }
-    if (this.data.password.length > 0 && this.data.password.length < 12) { this.errors.push("Password must be at least 12 characters.") }
-    if (this.data.password.length > 50) { this.errors.push("Password cannot exceed 50 characters.") }
-    if (this.data.username.length > 0 && this.data.username.length < 3) { this.errors.push("Username must be at least 3 characters.") }
-    if (this.data.username.length > 30) { this.errors.push("Username cannot exceed 30 characters.") }
+    return new Promise(async (resolve, reject) => {
+        if (this.data.username == "") { this.errors.push("You must provide a username.") }
+        if (this.data.username != "" && !validator.isAlphanumeric(this.data.username)) { this.errors.push("Username can only contain letters and numbers.") }
+        if (!validator.isEmail(this.data.email)) { this.errors.push("You must provide a valid email address.") }
+        if (this.data.password == "") { this.errors.push("You must provide a password.") }
+        if (this.data.password.length > 0 && this.data.password.length < 12) { this.errors.push("Password must be at least 12 characters.") }
+        if (this.data.password.length > 50) { this.errors.push("Password cannot exceed 50 characters.") }
+        if (this.data.username.length > 0 && this.data.username.length < 3) { this.errors.push("Username must be at least 3 characters.") }
+        if (this.data.username.length > 30) { this.errors.push("Username cannot exceed 30 characters.") }
+
+        // Only if username is valid then check to see if it's already taken
+        if (this.data.username.length > 2 && this.data.username.length < 31 && validator.isAlphanumeric(this.data.username)) {
+            let usernameExists = await usersCollection.findOne({ username: this.data.username })
+            if (usernameExists) { this.errors.push("Username already taken.") }
+        }
+
+        // Only if email is valid then check to see if it's already taken
+        if (validator.isEmail(this.data.email)) {
+            let emailExists = await usersCollection.findOne({ email: this.data.email })
+            if (emailExists) { this.errors.push("Email already being used.") }
+        }
+        resolve()
+    })
 }
 
 User.prototype.login = function () {
